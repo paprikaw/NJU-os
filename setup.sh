7bc2c0aa76d70ee871a4edb9a68522236ce4d5da
eric white <ericwhite@ericdeMacBook-Pro.local>
2023-02-01 12:10:14 +0800

    remove unnecessary script

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script is used to set up the CMU devcontainer environment
-git submodule add git@github.com:paprikaw/my-devcontainers.git my-devcontainers
-
-ln -s my-devcontainers/NJU-OS .devcontainer
